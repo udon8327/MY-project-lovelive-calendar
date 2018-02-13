6b253c6656495096db0bc7db697b253106051d0e
Bohan Huang <udon8327@gmail.com>
2018-02-13 19:19:00 +0800

    ver1.0

--- js/index.js
+++ js/index.js
@@ -12,3 +12,38 @@ $(function(){
     }
   });
 })
+
+
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    googleCalendarApiKey: 'AIzaSyBA8owMc6-Ozf9T9BTAEWufMSdVbOwb3hs',
+    events: {
+        googleCalendarId: 'pnk42e02rmm3rtgd5f0aves4ac@group.calendar.google.com',
+        className: 'gcal-event' // an option!
+    }
+  });
+});
+window.onload = function ()
+{
+  function color(){
+    $('.fc-content span:contains("Aqours")').parent().parent().css('background-color','green');
+  }
+  color();
+  $('.fc-right').click(function(){
+    window.setTimeout(function() {
+      color();
+    }, 300);
+    window.setTimeout(function() {
+      color();
+    }, 500);
+    window.setTimeout(function() {
+      color();
+    }, 700);
+    window.setTimeout(function() {
+      color();
+    }, 1000);
+    window.setTimeout(function() {
+      color();
+    }, 2000);
+  });
+}
